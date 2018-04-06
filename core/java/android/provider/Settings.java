376e7ce34aaf4ce3cae8ce43109a9d83eb4bfa25
Ali B <abittin@gmail.com>
2018-04-06 16:28:25 +0300

    QS footer icon visibilities [1/2]
    Ref: https://github.com/crdroidandroid/android_frameworks_base/commit/f0256e94528b26f52b0444b16cb36e843497b04e
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7749,6 +7749,24 @@ public final class Settings {
         public static final String AMBIENT_SHOW_SETTINGS_ICONS = "ambient_show_settings_icons";
 
         /**
+         * Show Settings icon in QS Footer
+         * @hide
+         */
+        public static final String QS_FOOTER_SHOW_SETTINGS = "qs_footer_show_settings";
+
+        /**
+         * Show Edit icon in QS Footer.
+         * @hide
+         */
+        public static final String QS_FOOTER_SHOW_EDIT = "qs_footer_show_edit";
+
+        /**
+         * Show power menu icon in QS Footer.
+         * @hide
+         */
+        public static final String QS_FOOTER_SHOW_POWER_MENU = "qs_footer_show_power_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
