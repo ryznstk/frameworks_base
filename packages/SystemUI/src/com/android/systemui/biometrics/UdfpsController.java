2064c04b03183ab262f8ae618441a8ff1b8cb83b
ShevT <evgeny.shishkov@gmail.com>
2022-12-15 15:23:45 +0300

    SystemUI: Allow devices to disable Smart Pixels on UDFPS
     With SmartPixels enabled, UDFPS does not work well.
     The higher the percentage of pixels to be disabled,
     the worse UDFPS works.
    
     Fix this by disabling SmartPixels when UDFPS is working.
    
    Change-Id: Ic478aa5d3a541d1ce533cfce7dacfd8ddec99ad0
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -250,6 +250,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private UdfpsAnimation mUdfpsAnimation;
     private boolean mKeyguardCallbackRegistered = false;
 
+    private boolean mDisableSmartPixels;
+    private boolean mSmartPixelsFlag;
+    private boolean mSmartPixelsEnabled;
+
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
             new VibrationAttributes.Builder()
@@ -271,6 +275,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private final ScreenLifecycle.Observer mScreenObserver = new ScreenLifecycle.Observer() {
         @Override
         public void onScreenTurnedOn() {
+            if (mDisableSmartPixels) {
+                isSmartPixelsEnabled();
+            }
             mScreenOn = true;
             if (mAodInterruptRunnable != null) {
                 mAodInterruptRunnable.run();
@@ -880,6 +887,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mWakefulnessLifecycle.addObserver(mWakefulnessLifecycleObserver);
         }
 
+        mDisableSmartPixels = mContext.getResources().getBoolean(com.android.systemui.res.R.bool.config_disableSmartPixelsOnUDFPS);
+
         mUseMtkGhbmDimming = mContext.getResources().getBoolean(
             com.android.systemui.res.R.bool.config_udfpsMtkGhbmDimming);
         if (com.android.internal.util.lunaris.Utils.isPackageInstalled(mContext,
@@ -897,6 +906,30 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
         if (mUdfpsAnimation != null) {
             mUdfpsAnimation.updatePosition();
+        }
+    }
+
+    private void isSmartPixelsEnabled() {
+        if (!mSmartPixelsFlag) {
+            mSmartPixelsEnabled = Settings.Secure.getIntForUser(
+                    mContext.getContentResolver(), Settings.Secure.SMART_PIXEL_FILTER_ENABLED,
+                    0, mContext.getUserId()) != 0;
+        }
+    }
+
+    private void disableSmartPixels() {
+        if (mSmartPixelsEnabled) {
+            Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SMART_PIXEL_FILTER_ENABLED,
+                    0, mContext.getUserId());
+        }
+    }
+
+    private void enableSmartPixels() {
+        if (mSmartPixelsEnabled) {
+            Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SMART_PIXEL_FILTER_ENABLED,
+                    1, mContext.getUserId());
         }
     }
 
@@ -1219,6 +1252,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             view.getViewRootImpl().notifyRendererOfExpensiveFrame();
         }
 
+        if (mDisableSmartPixels) {
+            if (!mSmartPixelsFlag && mSmartPixelsEnabled) {
+                disableSmartPixels();
+            }
+            mSmartPixelsFlag = true;
+        }
+
         if (isOptical()) {
             mLatencyTracker.onActionStart(ACTION_UDFPS_ILLUMINATE);
         }
@@ -1315,6 +1355,14 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mExecution.assertIsMainThread();
         mActivePointerId = MotionEvent.INVALID_POINTER_ID;
         mAcquiredReceived = false;
+
+        if (mDisableSmartPixels) {
+            if (mSmartPixelsFlag && mSmartPixelsEnabled) {
+                enableSmartPixels();
+            }
+            mSmartPixelsFlag = false;
+        }
+
         if (mOnFingerDown) {
             mFingerprintManager.onPointerUp(requestId, mSensorProps.sensorId, pointerId, x,
                     y, minor, major, orientation, time, gestureStart, isAod);
