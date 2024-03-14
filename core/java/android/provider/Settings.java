eeb0f17c34fca457fb149a69573eddae7f4eb2f5
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 19:07:49 +0800

    EdgeEffect: Implement edge scroll fling haptics
    squashed:
    
    	From: minaripenguin <minaripenguin@users.noreply.github.com>
    	Date: Sat, 7 Dec 2024 17:13:14 +0800
    	Subject: [PATCH] EdgeEffect: Add rate limit to vibration haptic effect
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Change-Id: I65a1a13788cd4fd5b0a51bd68b4ee32bc01e9b70
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7567,6 +7567,12 @@ public final class Settings {
         public static final String WIRELESS_CHARGING_RIPPLE_SHAPE = "wireless_charging_ripple_shape";
 
         /**
+         * @hide
+         */
+        @Readable
+        public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
