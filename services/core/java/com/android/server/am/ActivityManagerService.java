abf4cbb3519e9c5b5a37a5e2893bad49ae4e95fa
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-30 06:22:04 +0800

    services: Adding process freezer manager
    from code linaro:
    Freezer is added to enhance app launch and optimize app launch/exit
    animation. It mainly offload BG CPU resource by freezing other
    processes during app launch.
    
    Change-Id: I7eb44e0f5b3ec443b7858f790f0f1bad9615ad71
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -983,6 +983,10 @@ public class ActivityManagerService extends IActivityManager.Stub
         final int pid = app.getPid();
         synchronized (mPidsSelfLocked) {
             mPidsSelfLocked.doAddInternal(pid, app);
+            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
+            if (freezer != null && freezer.useFreezerManager()) {
+                freezer.addPidLocked(app);
+            }
         }
         synchronized (sActiveProcessInfoSelfLocked) {
             if (app.processInfo != null) {
@@ -1004,6 +1008,11 @@ public class ActivityManagerService extends IActivityManager.Stub
         final boolean removed;
         synchronized (mPidsSelfLocked) {
             removed = mPidsSelfLocked.doRemoveInternal(pid, app);
+            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
+            if (freezer != null && freezer.useFreezerManager()) {
+                freezer.removePidLocked(pid, app);
+                freezer.startUnfreeze(app.processName, ProcessFreezerManager.REMOVE_PROCESS_UNFREEZE);
+            }
         }
         if (removed) {
             synchronized (sActiveProcessInfoSelfLocked) {
@@ -5390,6 +5399,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         showMteOverrideNotificationIfActive();
 
         t.traceEnd();
+        ProcessFreezerManager.getInstance().init(mFreezer);
     }
 
     private void showConsoleNotificationIfActive() {
