d58b070ddcb47cecd45b765cfad756b4ff72fbc4
Ghosuto <clash.raja10@gmail.com>
2025-11-16 09:19:53 +0000

    SystemUI: Introduce Lockscreen Now playing
    Change-Id: Ia1323c3ebce854003b85d5fe904dd7b2aad9db63
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -135,6 +135,7 @@ import com.android.systemui.demomode.DemoMode;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent;
 import com.android.systemui.edgelight.EdgeLightViewController;
+import com.android.systemui.nowplaying.NowPlayingViewController;
 import com.android.systemui.emergency.EmergencyGesture;
 import com.android.systemui.emergency.EmergencyGestureModule.EmergencyGestureIntentFactory;
 import com.android.systemui.flags.FeatureFlags;
@@ -481,6 +482,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final MediaViewController mMediaViewController;
     private final PulseViewController mPulseViewController;
     private final EdgeLightViewController mEdgeLightViewController;
+    private final NowPlayingViewController mNowPlayingViewController;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -749,7 +751,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             WindowManagerProvider windowManagerProvider,
             MediaViewController mediaViewController,
             PulseViewController pulseViewController,
-           EdgeLightViewController edgeLightViewController,
+            EdgeLightViewController edgeLightViewController,
+            NowPlayingViewController nowPlayingViewController,
             BurnInProtectionController burnInProtectionController
     ) {
         mContext = context;
@@ -900,6 +903,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mMediaViewController = mediaViewController;
         mPulseViewController = pulseViewController;
         mEdgeLightViewController = edgeLightViewController;
+        mNowPlayingViewController = nowPlayingViewController;
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1143,6 +1147,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         getNotifContainerParentView().addView(mMediaViewController.getMediaArtScrim(), 0);
         getNotifContainerParentView().addView(mPulseViewController.getPulseView(), 1);
         getNotifContainerParentView().addView(mEdgeLightViewController.getEdgeLightView(), 2);
+        getNotifContainerParentView().addView(mNowPlayingViewController.getNowPlayingView(), 3);
     }
 
     private ViewGroup getNotifContainerParentView() {
