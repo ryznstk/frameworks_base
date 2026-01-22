47028e749e4dc3af7ba2c15449ad393d241e9b8c
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-22 19:20:17 +0100

    SystemUI: Add notification row transluency toggle
    Change-Id: I4ea5385caf72336bd8fbc87f85e3e00e85d189d2
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14454,6 +14454,12 @@ public final class Settings {
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
         /**
+         * Translucent notifications
+         * @hide
+         */
+        public static final String NOTIFICATION_ROW_TRANSPARENCY = "notification_row_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
