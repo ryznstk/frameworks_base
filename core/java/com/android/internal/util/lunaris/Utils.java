d2e03ea488773c2da46675a04150230837b8f5a2
Blaster4385 <TheTablaster@aospa.co>
2026-02-10 11:38:22 +0000

    base: Add util function to restart any app
    - Based onn [1]
    - Nuked uneeded code
    - Modify code to not restrict function to single app
    
    [1] PixysOS/frameworks_base@f479a95
    
    Change-Id: I926236a1f9ef281b827089466285413bd1f7358a
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/com/android/internal/util/lunaris/Utils.java
+++ core/java/com/android/internal/util/lunaris/Utils.java
@@ -16,6 +16,9 @@
 
 package com.android.internal.util.lunaris;
 
+import android.app.ActivityManager;
+import android.app.IActivityManager;
+import android.content.Context;
 import android.content.Context;
 import android.content.Intent;
 import android.content.om.OverlayManager;
@@ -25,6 +28,8 @@ import android.content.om.OverlayInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.AsyncTask;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -41,6 +46,39 @@ import java.util.List;
 public class Utils {
 
     private static final String TAG = "Utils";
+
+    public static void restartApp(String appName, Context context) {
+        new RestartAppTask(appName, context).execute();
+    }
+
+    private static class RestartAppTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+        private String mApp;
+
+        public RestartAppTask(String appName, Context context) {
+            super();
+            mContext = context;
+            mApp = appName;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if (mApp.equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
     public static boolean isPackageInstalled(Context context, String packageName, boolean ignoreState) {
         if (packageName != null) {
