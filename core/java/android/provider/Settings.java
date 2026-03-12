f0b76a25d865b6cf6bb515816b246a14f6662137
kenway214 <kenway214@outlook.com>
2026-03-12 13:10:35 +0000

    core: Introduce per-apps spoofing [1/2]
    - Device models taken from PixelPropsUtils
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14575,6 +14575,32 @@ public final class Settings {
         public static final String NAVBAR_IME_SPACE = "navbar_ime_space";
 
         /**
+         * Per-apps device spoofing
+         * @hide
+         */
+        @Readable
+        public static final String PER_APPS_DEVICE_SPOOF = "per_apps_device_spoof";
+
+        /**
+         * Custom device spoof profiles for per-app spoofing
+         * @hide
+         */
+        @Readable
+        public static final String CUSTOM_SPOOF_PROFILES = "custom_spoof_profiles";
+
+        /** 
+         * @hide 
+         */
+        @Readable
+        public static final String PER_APPS_DEVICE_SPOOF_ENABLED = "per_apps_device_spoof_enabled";
+
+        /** 
+         * @hide 
+         */
+        @Readable
+        public static final String PER_APPS_DEVICE_SPOOF_CACHE = "per_apps_device_spoof_cache";
+
+        /**
          * Whether to use system accent color for lock screen clock text
          * @hide
          */
