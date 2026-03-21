d3fc131d04056bfaf3a89c43b3e677a6b689375b
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-22 00:08:30 +0800

    Add support for game space
    Change-Id: Iaaf20bcbaa50c42dd6b7739ae233b245a2c5ad5c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/AxExtServiceFactory.java
+++ services/core/java/com/android/server/AxExtServiceFactory.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import com.android.server.am.*;
 import com.android.server.pm.*;
 import com.android.server.wm.AxSandboxService;
+import com.android.server.wm.GameSpaceService;
 import com.android.server.wm.WindowManagerService;
 
 public class AxExtServiceFactory {
@@ -70,6 +71,7 @@ public class AxExtServiceFactory {
 
     public static void systemReady() {
         AxSandboxService.systemReady();
+        GameSpaceService.systemReady();
     }
     
     public static void onLateSystemReady() {
