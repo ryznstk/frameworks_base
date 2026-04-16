cef7583f69d643b84feb70705ed03d3d01968727
Ghosuto <clash.raja10@gmail.com>
2026-04-16 19:00:58 +0000

    SystemUI: User selected custom statusbar logo [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7184,6 +7184,12 @@ public final class Settings {
         public static final String STATUS_BAR_LOGO_COLOR_PICKER = "status_bar_logo_color_picker";
 
         /**
+         * Statusbar logo custom img
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO_CUSTOM_IMAGE_URI = "status_bar_logo_custom_image_uri";
+
+        /**
          * Whether to display cross sign for a data disabled connection
          * @hide
          */
