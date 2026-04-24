6eac19c4174e936bc51a8740ba9f95f802ebc634
Ghosuto <clash.raja10@gmail.com>
2026-04-24 09:58:34 +0000

    SystemUI: Redesign QS panel Widget
    - 2 music player style
    - slider corner style
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7026,6 +7026,21 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String QS_WIDGET_PANEL = "qs_widget_panel";
+
+        /**
+         * @hide
+         */
+        public static final String QS_WIDGET_IOS_MUSIC = "qs_widget_ios_music";
+
+        /**
+         * @hide
+         */
+        public static final String QS_WIDGET_SLIDER_CORNER = "qs_widget_slider_corner";
+
+        /**
+         * @hide
+         */
         public static final String SINGLE_QS_TONE_ENABLED = "single_qs_tone_enabled";
 
         /**
