69a89648ae4298e16437d19ddb6ae8621dce34e0
ruyue <ruyueee@outlook.com>
2026-04-28 19:24:48 +0000

    SystemUI: Allow toggling keyguard behind scrim visibility [1/2]
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15330,6 +15330,12 @@ public final class Settings {
         public static final String KEYGUARD_AFFORDANCE_SINGLE_TAP = "keyguard_affordance_single_tap";
 
         /**
+         * keyguard scrim
+         * @hide
+         */
+        public static final String KEYGUARD_SCRIM_TRANSPARENT = "keyguard_scrim_transparent";
+
+        /**
          * Whether to enable Sleep Mode
          * @hide
          */
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -34,6 +34,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.util.MathUtils;
 import android.util.Pair;
@@ -1119,9 +1120,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             int behindTint = result.first;
             float behindAlpha = result.second;
 
-            if (mState == ScrimState.KEYGUARD) {
+            if (mState == ScrimState.KEYGUARD && Settings.Secure.getInt(
+                    mContext.getContentResolver(),
+                    Settings.Secure.KEYGUARD_SCRIM_TRANSPARENT,
+                    1) != 0) {
                 behindAlpha = 0.0f;
-                behindTint = Color.TRANSPARENT; 
+                behindTint = Color.TRANSPARENT;
             }
             
             if (mTransitionToFullShadeProgress > 0.0f) {
