c89360c248f8a82c65d93ce599d713bbb565631e
Pranav Vashi <neobuddy89@gmail.com>
2026-04-28 07:02:32 +0530

    SystemUI: DynamicBar: Hide weather and smartspace on expand
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/AxDynamicBarKeyguardChipSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/AxDynamicBarKeyguardChipSection.kt
@@ -41,6 +41,11 @@ private val HIDDEN_VIEW_IDS = listOf(
     R.id.keyguard_widgets,
     R.id.shared_notification_container,
     R.id.notificationShelf,
+    R.id.bc_smartspace_view,
+    R.id.smartspace_card_pager,
+    R.id.smartspace_page_indicator,
+    R.id.keyguard_slice_view,
+    R.id.keyguard_weather_area,
 )
 
 private fun Float.dpToPx(context: Context): Int =
