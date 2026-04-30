a896717ea6c38d40f70d35856a729b1576c147f7
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-01 07:28:58 +0800

    services: Optimizing navigation bar draws
    Change-Id: I133eff520b8a45b6e1e197d9b37f60fae1b17bc9
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -1752,7 +1752,8 @@ public class DisplayPolicy {
             // mode; if it's in gesture navigation mode, the navigation bar will be
             // NAV_BAR_FORCE_TRANSPARENT and its appearance won't be decided by overlapping
             // windows.
-            if (win.isOverlappingWithNavBar()) {
+            if ((mNavBarColorWindowCandidate == null || mNavBarBackgroundWindowCandidate == null)
+                    && win.isOverlappingWithNavBar()) {
                 if (mNavBarColorWindowCandidate == null) {
                     mNavBarColorWindowCandidate = win;
                     addSystemBarColorApp(win);
@@ -2950,8 +2951,9 @@ public class DisplayPolicy {
         final Rect df = displayFrames.mUnrestricted;
         final Rect safe = sTmpDisplayCutoutSafe;
         final Insets waterfallInsets = state.getDisplayCutout().getWaterfallInsets();
-        final Rect outRect = new Rect();
+        final Rect outRect = sTmpRect2;
         final Rect sourceContent = sTmpRect;
+        outRect.setEmpty();
         safe.set(displayFrames.mDisplayCutoutSafe);
         for (int i = state.sourceSize() - 1; i >= 0; i--) {
             final InsetsSource source = state.sourceAt(i);
@@ -3019,6 +3021,9 @@ public class DisplayPolicy {
             final WindowState window = mStatusBarBackgroundWindows.get(i);
             drawBackground &= drawsBarBackground(window, Type.statusBars());
             isFullyTransparentAllowed &= isFullyTransparentAllowed(window, Type.statusBars());
+            if (!drawBackground && !isFullyTransparentAllowed) {
+                break;
+            }
         }
 
         if (drawBackground) {
